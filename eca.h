8676421a265cc454702bfe732266234eb3419ded
Vitalie Ciubotaru <vitalie@ciubotaru.tk>
2017-01-07 23:03:00 +0900

    Added function prototypes in eca.h

---
+++ eca.h
@@ -0,0 +1,19 @@
+/**
+ * File name: eca.h
+ * Project name: bitfield-eca, an implementation of elementary cellular
+ * automata written in C.
+ * URL: https://github.com/ciubotaru/bitfield-eca
+ * Author: Vitalie Ciubotaru <vitalie at ciubotaru dot tk>
+ * License: General Public License, version 3 or later
+ * Copyright 2017
+**/
+
+#ifndef BITFIELD_ECA_H
+#define BITFIELD_ECA_H
+#include <bitfield.h>
+
+void eca_string_ip(struct bitfield *instance, const unsigned int wolfram_code);	/* transforms an array of bits as an elementary cellular automaton given by Wolfram's code "in-place" */
+
+struct bitfield *eca_string(const struct bitfield *input, const unsigned int wolfram_code);	/* transforms an array of bits as an elementary cellular automaton given by Wolfram's code */
+
+#endif
